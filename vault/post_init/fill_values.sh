9fdb37239015f2a935dbde6f67271ca6da8aa353
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-10-14 19:32:50 +0000

    -

--- vault/post_init/fill_values.sh
+++ vault/post_init/fill_values.sh
@@ -13,6 +13,10 @@ vault kv put secret/pyrus PYRUS.creds.login=plogin PYRUS.creds.security_key=skey
 vault kv put secret/rpa USER=user PASSWORD=passwd
 vault kv put secret/docutable DOCA.SOMETHING=SOMETHING
 
+vault kv put secret/web OIDC_RP_SIGN_ALGO=RS256
+vault kv put secret/hl HOTFOLDER.inpath=docs-map.yaml
+vault kv put secret/pyin DOCTYPE_OVERRIDES.bankruptcy.DREAMDOCS.INFO.DOCSET=bankrotstvo-zaiavlenie-test
+
 
 vault auth enable userpass
 vault write auth/userpass/users/myusername password=mypassword policies=admin
