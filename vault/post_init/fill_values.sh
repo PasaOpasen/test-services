4e45d45341e7704c98c599ba71c28d870d3cb6c9
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-10-14 09:13:46 +0000

    -

--- vault/post_init/fill_values.sh
+++ vault/post_init/fill_values.sh
@@ -1,8 +1,13 @@
 
+set -e
+
 export VAULT_ADDR='http://0.0.0.0:8200'
 # export VAULT_ADDR='http://vault.db:8200'
 
+# export VAULT_NAMESPACE=admin
+
 vault login root
+vault policy write admin /post_init/admin-policy.hcl
 
 vault kv put secret/pyrus PYRUS.creds.login=plogin PYRUS.creds.security_key=skey
 vault kv put secret/rpa USER=user PASSWORD=passwd
@@ -10,7 +15,9 @@ vault kv put secret/docutable DOCA.SOMETHING=SOMETHING
 
 
 vault auth enable userpass
-vault write auth/userpass/users/myusername password=mypassword policies=vault-quickstart-policy
+vault write auth/userpass/users/myusername password=mypassword policies=admin
 
-#hvac_client = hvac.Client(url="http://localhost:8200")
-#hvac_client.auth_userpass(username="myusername", password="mypassword")
+# vault login -method=userpass username=myusername password=mypassword
+
+
+
