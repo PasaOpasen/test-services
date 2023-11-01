417a328c59ae8b72d8768daa84581044d9256a9b
Demetry Pascal <qtckpuhdsa@gmail.com>
2023-11-01 08:16:46 +0000

    -

--- vault/post_init/fill_values.sh
+++ vault/post_init/fill_values.sh
@@ -23,5 +23,21 @@ vault write auth/userpass/users/myusername password=mypassword policies=admin
 
 # vault login -method=userpass username=myusername password=mypassword
 
+vault auth enable approle
+
+vault write auth/approle/role/custom-role \
+    secret_id_ttl=0m \
+    token_num_uses=0 \
+    token_ttl=0m \
+    token_max_ttl=0m \
+    secret_id_num_uses=40 \
+    policies="admin"
 
 
+vault read auth/approle/role/custom-role/role-id
+
+vault write -f auth/approle/role/custom-role/secret-id
+
+# vault write auth/approle/login \
+#     role_id=18035f28-e0a1-d811-d439-6df236f5dfd9 \
+#     secret_id=95b0f4d4-fdae-da7e-6580-0f516f69c27b
